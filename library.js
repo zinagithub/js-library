c54e76e93a0bcf28853c6ab8558f731a00eea7c9
YellowPipe <philippdmitrienko@gmail.com>
2019-07-02 13:49:40 +0300

    refactor to ES6, minor bug fixes

--- library.js
+++ library.js
@@ -1,53 +1,64 @@
 let myLibrary = [];
 
-var info1 = document.getElementById("getBookForm");
-var info2 = document.getElementById("displayBooks");
-var but1   = document.getElementById("addButton");
-var but2   = document.getElementById("addBook");
+const info1 = document.getElementById("getBookForm");
+const info2 = document.getElementById("displayBooks");
+const but1   = document.getElementById("addButton");
+const but2   = document.getElementById("addBook");
 
+class Book {
+  constructor(title, author, pages,read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+  }
 
-function Book(title, author, pages,read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+  toggleStatus() {
+    this.read = !(this.read)
+  }
+
+  changeReadStatus() {
+    let myInput = Number(this.id[1]);
+    myLibrary[myInput].toggleStatus();
+    localStorage.clear();
+      setMyLibrary();
+  }
+
 }
 
-Book.prototype.toggleStatus = function() {
-  this.read = !(this.read)
-}
+const saveBook = () => {
+	const title  = document.getElementById("title").value;
+	const author = document.getElementById("author").value;
+	const nbPages= document.getElementById("nbPages").value;
+	const read   = document.getElementById("checked").checked;
 
-function saveBook(){
-	var title  = document.getElementById("title").value;
-	var author = document.getElementById("author").value;
-	var nbPages= document.getElementById("nbPages").value;
-	var read   = document.getElementById("checked").checked;
-
-	var book1 = new Book(title, author,nbPages,read);
+	const book1 = new Book(title, author,nbPages,read);
     myLibrary.push(book1);
     setMyLibrary()
 }
 
-function getMyLibrary(){
-	let myLibrary_deserialized = JSON.parse(localStorage.getItem("myLibrary")).map((book) =>
+const setMyLibrary = () => {
+  let myLibrary_serialized =JSON.stringify(myLibrary)
+  localStorage.setItem("myLibrary",myLibrary_serialized);
+}
+
+const getMyLibrary = () => {
+  let myLibrary_deserialized
+  if (localStorage.getItem("myLibrary")) {
+    myLibrary_deserialized = JSON.parse(localStorage.getItem("myLibrary")).map((book) =>
       Object.assign(new Book(), book),
     );
+  }
 	return myLibrary_deserialized
-
 }
 
-function setMyLibrary(){
-	let myLibrary_serialized =JSON.stringify(myLibrary)
-	localStorage.setItem("myLibrary",myLibrary_serialized);
-}
-
-function render(){
+const render = () => {
     let  objLibrary = getMyLibrary();
 
-    if (objLibrary.length > 0){
+    if (objLibrary && objLibrary.length){
       	myLibrary = objLibrary
-      	var mytext = "";
-      	for (var i = 0;i < myLibrary.length;i++){
+      	let mytext = "";
+      	for (let i = 0;i < myLibrary.length;i++){
     		  mytext += "<div class = 'card'><h3>Title : "+myLibrary[i].title+
     		"</h3><p>Author : "+myLibrary[i].author+"</p><p>Pages : "+myLibrary[i].pages+
     		"</p>";
@@ -63,22 +74,15 @@ function render(){
         info4 = document.getElementsByClassName("status");
         for (i = 0;i<info3.length;i++){
     		info3[i].addEventListener("click",deleteBook);
-    		info4[i].addEventListener("click",changeReadStatus);
+    		info4[i].addEventListener("click", Book.changeReadStatus);
 		}
-    }else{
+    } else {
     	info2.innerText = "Empty Library"
     }
     
 }
 
-function changeReadStatus(){
-	let myInput = Number(this.id[1]);
-	myLibrary[myInput].toggleStatus();
-	localStorage.clear();
-  	setMyLibrary();
-
-}
-function deleteBook(){
+const deleteBook = () => {
    let conf = confirm("are you sure?");
    if (conf){
    	myLibrary.splice(this.id,1);
@@ -88,12 +92,12 @@ function deleteBook(){
    }  
 }
 
-function addBookToLibrary() {
+const addBookToLibrary = () => {
     info1.style.display = "block";
     but1.addEventListener("click",saveBook);
 }
 
-function removeAllBook(){
+const removeAllBook = () => {
   let conf = confirm("Are you sure you want to clear your library?");
   if (conf) {
     localStorage.clear();
